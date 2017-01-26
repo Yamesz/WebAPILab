b05ec419200a465d99768008daa2d1e576095661
林楊森(網技二課) <yangsen.lin@evertrust.com.tw>
2017-01-26 11:22:03 +0800

    加入 HttpResponseMessageDemo 範例

---
+++ WebAPILab/Controllers/HttpResponseMessageDemoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using static WebAPILab.Controllers.ProductsController;
+
+namespace WebAPILab.Controllers
+{
+    public class HttpResponseMessageDemoController : ApiController
+    {
+        [Route("HttpResponseMessage/Demo1")]
+        public HttpResponseMessage GetDemo1()
+        {
+            var response = Request.CreateResponse<Demo2>(
+                    HttpStatusCode.Created,
+                    new Demo2(),
+                    new JsonMediaTypeFormatter() { Indent = true }, "text/json");
+            response.ReasonPhrase = "YC";
+            
+            return response;
+        }
+    }
+}
