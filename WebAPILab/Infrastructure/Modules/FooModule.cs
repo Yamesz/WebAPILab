627ca86b690348061dd8e347bb5711ed423d5c78
g1664 <g1664>
2017-02-16 09:37:44 +0800

    實作 IHttpModule

---
+++ WebAPILab/Infrastructure/Modules/FooModule.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace WebAPILab.Infrastructure.Modules
+{
+    public class FooModule : IHttpModule
+    {
+        private HttpApplication Context { get; set; }
+
+        public void Init(HttpApplication context)
+        {
+            this.Context = context;
+
+            context.BeginRequest +=
+                new EventHandler(this.context_BeginRequest);
+
+            context.PreSendRequestHeaders +=
+                new EventHandler(this.context_PreSendRequestHeaders);
+        }
+
+        private void context_BeginRequest(object sender, EventArgs e)
+        {
+            HttpApplication httpApplication = sender as HttpApplication;
+            Debug.WriteLine("FooModule - BeginRequest");
+       
+        }
+
+        private void context_PreSendRequestHeaders(object sender, EventArgs e)
+        {
+            HttpApplication httpApplication = sender as HttpApplication;
+            Debug.WriteLine("FooModule - PreSendRequestHeaders");
+           
+        }
+
+      
+
+        public void Dispose()
+        {
+            
+        }
+
+       
+    }
+}
---
+++ WebAPILab/Infrastructure/Modules/RequestProcessModule.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace WebAPILab.Infrastructure.Modules
+{
+    public class RequestProcessModule : IHttpModule
+    {
+        private HttpApplication Context { get; set; }
+
+        public void Init(HttpApplication context)
+        {
+            this.Context = context;
+
+            context.BeginRequest +=
+                new EventHandler(this.context_BeginRequest);
+
+            context.PreSendRequestHeaders +=
+                new EventHandler(this.context_PreSendRequestHeaders);
+        }
+
+        private void context_BeginRequest(object sender, EventArgs e)
+        {
+            HttpApplication httpApplication = sender as HttpApplication;
+            Debug.WriteLine("context_BeginRequest");
+            this.RestrictRequestPath(httpApplication);
+        }
+
+        private void context_PreSendRequestHeaders(object sender, EventArgs e)
+        {
+            HttpApplication httpApplication = sender as HttpApplication;
+            Debug.WriteLine("context_PreSendRequestHeaders");
+            this.RestrictRequestPath(httpApplication);
+        }
+
+        private void RestrictRequestPath(HttpApplication httpApplication)
+        {
+            HttpContextBase httpContextBase = new HttpContextWrapper(httpApplication.Context);
+        
+            var requestPath = httpApplication.Request.Path.ToLower();
+            Debug.WriteLine($"requestPath = {requestPath}");
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+       
+    }
+}
